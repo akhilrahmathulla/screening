b914cbf6bc1b8ea39f59f873ebdad95afcb95067
akhilrahmathulla <157045094+akhilrahmathulla@users.noreply.github.com>
2024-01-18 22:01:29 +0530

    Update main.go

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 )
 
 func main() {
+	// function name should be start with capital letter
 	setupJsonApi()
+	// add NewRoute() to return new route instance and add healthcheck() to add liveness and readness
 	http.ListenAndServe(":80", nil)
 }
